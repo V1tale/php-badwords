aa58c523c69ae2b307a953f84a443841220e0c0a
V1tale <elativ.c@gmail.com>
2022-11-24 16:04:41 +0100

    completato

--- index.php
+++ index.php
@@ -8,15 +8,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- FRAMEWORK -->
+    <link rel="stylesheet" href="css/utility-framework.css">
+    <!-- STYLE -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Bad Words</title>
 </head>
 <body>
-    <form action="resp.php" method="GET">
-        <label for="paragraph"></label>
-        <textarea name="text" id="paragraph" cols="30" rows="10"></textarea>
-        <label for="censored"></label>
-        <input type="text" id="censored" name="censored">
-        <button>Get Started!</button>
-    </form>
+    <header>
+        <div class="container">
+            <h1><span>BAD </span>Words</h1>
+            <h2>Proteggi chi ami censurando parole sconce</h2>
+        </div>
+        </header>
+        <main class="bgprussianblue">
+            <div class="container ">               
+                <p>Sei uno di quei "genitori elicottero?" pensi che tuo figlio sia sul punto di entrare in una gang criminale solo perchè si esprime in maniera inopportuna? Grazie a questa form, potrai correggere tutti i suoi compiti di scuola, censurando ogni parola incline alla vita da strada. Non ti aiuterà a correggere il suo modo di parlare o aiutarlo nelle frequentazioni, ma almeno potrai dire di averci provato, quando farai la vittima parlando al bar con i tuoi amici.</p>
+                <h3>Riempi la form ed invia il testo con le parole da censurare. Ti diremo anche quando è lungo il paragrafo, nel caso dovesse tornarti utile</h3>
+                <form action="resp.php" method="GET">
+                    <label for="paragraph">Inserisci il testo</label>
+                    <textarea name="text" id="paragraph" cols="30" rows="10"></textarea>
+                    <label for="censored">Inserisci la parola che vuoi censurare</label>
+                    <input type="text" id="censored" name="censored">
+                    <button>Get Started!</button>
+                </form>
+            </div>
+        </main>
+    </div>
 </body>
 </html>
--- resp.php
+++ resp.php
@@ -10,18 +10,34 @@ $finalText = str_replace($censored, "***", $text);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- FRAMEWORK -->
+    <link rel="stylesheet" href="css/utility-framework.css">
+    <!-- STYLE -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Bad Words</title>
 </head>
 <body>
-    <div class="container">
-        <div class="top">
-            <p><?php echo $text;?></p>
-            <h4><?php echo strlen($text); ?></h4>
+    <header>
+        <div class="container">
+            <h1><span>BAD </span>Words</h1>
+            <h2>Proteggi chi ami censurando parole sconce</h2>
         </div>
-        <div class="bottom">
-            <p><?php  echo $finalText;?></p>
-            <h4><?php echo strlen($finalText); ?></h4>
+    </header>
+    <main class="bgprussianblue">       
+        <div class="container">
+            <div class="top">
+                <h3>Il testo che hai inserito:</h3>
+                <p><?php echo $text;?></p>
+                <h3>La quantità di lettere presenti nel testo:</h3>
+                <h4><?php echo strlen($text); ?></h4>
+            </div>
+            <div class="bottom">
+                <h3>Il testo censurato:</h3>
+                <p><?php  echo $finalText;?></p>
+                <h3>La quantità di caratteri nel testo censurato:</h3>
+                <h4><?php echo strlen($finalText); ?></h4>
+            </div>
         </div>
-    </div>
+    </main>
 </body>
 </html>
